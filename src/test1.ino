5f65cf26a1fe52f54333c56e6ded68813195a019
Etsushi Nozaki <nozaetsu0918@gmail.com>
2017-08-24 14:31:43 +0900

    WIP: Inplement LED toggle button control

--- src/test1.ino
+++ src/test1.ino
@@ -1,12 +1,26 @@
 const int LED_PIN = 5;
+const int BUTTON_PIN = 4;
+
+bool output_mode; // Turn on LED when true
 
 void setup () {
+    pinMode(BUTTON_PIN, INPUT_PULLUP);
     pinMode(LED_PIN, OUTPUT);
+    output_mode = false;
 }
 
 void loop() {
-    for (auto i = 0; i < 255; ++i) {
-        analogWrite(LED_PIN, i);
-        delay(20);
+    if (digitalRead(BUTTON_PIN) == false) {
+        output_mode = !output_mode;
+    }
+
+    if (output_mode == true) {
+        for (auto i = 0; i < 255; ++i) {
+            analogWrite(LED_PIN, i);
+            delay(20);
+        }
+    }
+    else {
+        analogWrite(LED_PIN, 0);
     }
 }
