9b3b8d058853b3987d5d792942e0782cdd88c4b7
Etsushi Nozaki <nozaetsu0918@gmail.com>
2017-08-25 00:01:09 +0900

    Implement LCD (ST7032i) demo

--- src/test1.ino
+++ src/test1.ino
@@ -1,4 +1,6 @@
 #include <MsTimer2.h>
+#include <Wire.h>
+#include <ST7032.h>
 
 const int LED_PIN = 5;
 const int BUTTON_PIN = 2;
@@ -7,28 +9,28 @@ const int PERIOD = 3000;
 volatile bool output_mode; // Turn on LED when true
 long count = 0;
 
+ST7032 lcd;
+
 void setup () {
     pinMode(BUTTON_PIN, INPUT_PULLUP);
     pinMode(LED_PIN, OUTPUT);
+
     output_mode = false;
     attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), toggle_LED, FALLING);
     Serial.begin(9600);
     Serial.println("Serial communication start.");
     MsTimer2::set(PERIOD, print);
     MsTimer2::start();
-
+    // set up the LCD's number of columns and rows:
+    lcd.begin(16, 2);
+    lcd.setContrast(10);
+    lcd.print("hello, world!");
 }
 
 void loop() {
-    for (auto i = 0; i < 255; ++i) {
-        if (output_mode == true) {
-            analogWrite(LED_PIN, i);
-        }
-        else {
-            analogWrite(LED_PIN, 0);
-        }
-        delay(20);
-    }
+    lcd.setCursor(0, 1);
+    // print the number of seconds since reset:
+    lcd.print(millis()/1000);
 }
 
 void toggle_LED() {
