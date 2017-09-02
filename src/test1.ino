925a15901681cb7e9abe80b8d6296df5ac29d88a
Etsushi Nozaki <nozaetsu0918@gmail.com>
2017-09-02 14:03:00 +0900

    ADT7410 13bit/16bit mode temperature acquisition

--- src/test1.ino
+++ src/test1.ino
@@ -2,6 +2,8 @@
 #include <Wire.h>
 #include <ST7032.h>
 
+#define ADT7410_16BIT
+
 const int LED_PIN = 5;
 const int BUTTON_PIN = 2;
 const int PERIOD = 3000;
@@ -27,6 +29,18 @@ void setup () {
     lcd.setContrast(10);
     lcd.print("hello, world!");
     Wire.begin();
+#ifdef ADT7410_16BIT
+    Wire.beginTransmission(ADT7410_ADDR);
+    Wire.write(0x03);
+    Wire.endTransmission(false);
+    Wire.requestFrom(ADT7410_ADDR, 1);
+    uint8_t config = Wire.read();
+    config |= 0x80;
+    Wire.beginTransmission(ADT7410_ADDR);
+    Wire.write(0x03);
+    Wire.write(config);
+    Wire.endTransmission();
+#endif
 }
 
 void loop() {
@@ -35,6 +49,14 @@ void loop() {
 
     Wire.requestFrom(ADT7410_ADDR, 2);
     temp_u16 = Wire.read() << 8 | Wire.read();
+#ifdef ADT7410_16BIT
+    if (temp_u16 & 0x8000) {
+        temp_f = temp_u16/128.0 - 512.0;
+    }
+    else {
+        temp_f = temp_u16/128.0;
+    }
+#else
     temp_u16 >>= 3;
 
     if (temp_u16 & 0x1000) {
@@ -43,6 +65,7 @@ void loop() {
     else {
         temp_f = temp_u16/16.0;
     }
+#endif
     lcd.setCursor(0, 1);
     // print the number of seconds since reset:
     lcd.print(temp_f);
