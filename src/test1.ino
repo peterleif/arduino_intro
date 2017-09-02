a4ad9ce9f698d74d2c610ed98fa056d5679aa367
Etsushi Nozaki <nozaetsu0918@gmail.com>
2017-09-02 13:39:24 +0900

    ADT7410 13bit mode temperature acquisition

--- src/test1.ino
+++ src/test1.ino
@@ -5,6 +5,7 @@
 const int LED_PIN = 5;
 const int BUTTON_PIN = 2;
 const int PERIOD = 3000;
+const int ADT7410_ADDR = 0x48;
 
 volatile bool output_mode; // Turn on LED when true
 long count = 0;
@@ -25,12 +26,27 @@ void setup () {
     lcd.begin(16, 2);
     lcd.setContrast(10);
     lcd.print("hello, world!");
+    Wire.begin();
 }
 
 void loop() {
+    uint16_t temp_u16;
+    float temp_f;
+
+    Wire.requestFrom(ADT7410_ADDR, 2);
+    temp_u16 = Wire.read() << 8 | Wire.read();
+    temp_u16 >>= 3;
+
+    if (temp_u16 & 0x1000) {
+        temp_f = temp_u16/16.0 - 512.0;
+    }
+    else {
+        temp_f = temp_u16/16.0;
+    }
     lcd.setCursor(0, 1);
     // print the number of seconds since reset:
-    lcd.print(millis()/1000);
+    lcd.print(temp_f);
+    delay(1000);
 }
 
 void toggle_LED() {
